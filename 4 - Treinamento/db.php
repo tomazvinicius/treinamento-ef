0e5612b16cb50e98f8d6702ac92c9adc33905fd4
Vinicius Tomaz <viniciustomax@gmail.com>
2023-09-22 16:24:01 -0400

    feat - validacoes

--- 4 - Treinamento/db.php
+++ 4 - Treinamento/db.php
@@ -43,7 +43,6 @@ class db
 
         $ok = pg_query($this->connection, "INSERT INTO $table ($colunas) VALUES ($valores) returning id_est");
         $ok = pg_fetch_all($ok);
-        $tipoTransacao_mov = "Inicial";
         $data_mov = (date("Y-m-d"));
         $quantidade_mov = $values['quantidade_est'];
         $id = $ok[0]['id_est'];
@@ -70,7 +69,7 @@ class db
 
         if ($values['tipotransacao_mov'] == 'Entrada') {
             $novaQuantidade = $queryEstoque[0]['quantidade_est'] + $values['quantidade_mov'];
-        } else if ($queryEstoque[0]['quantidade_est'] > 0 || $values['tipotransacao_mov'] == 'Saida') {
+        } else if ($queryEstoque[0]['quantidade_est'] >= 0 || $values['tipotransacao_mov'] == 'Saida') {
             $novaQuantidade = $queryEstoque[0]['quantidade_est'] - $values['quantidade_mov'];
         }
 
@@ -87,16 +86,19 @@ class db
     {
         $ok = pg_query($this->connection, "SELECT * FROM $table");
         system('clear');
-        echo "-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=\n";
-        echo "|  ID  |       Nome      | Quantidade |\n";
-        echo "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-\n\n";
 
+        echo "+------+---------------------------------------------+---------------+\n";
+        echo "|  ID  |                   Nome                      |     Estoque   |\n";
+        echo "+------+---------------------------------------------+---------------+\n";
+        $total = 0;
         while ($row = pg_fetch_assoc($ok)) {
+            $nome = mb_strcut($row["nome_est"], 0, 42);
             echo "| " . str_pad($row["id_est"], 4, " ", STR_PAD_LEFT) . " | ";
-            echo str_pad(substr($row["nome_est"], 0, 18), 15, " ", STR_PAD_RIGHT) . " | ";
-            echo str_pad($row["quantidade_est"], 10, " ", STR_PAD_RIGHT) . " |\n";
-            echo "_______________________________________\n\n";
+            echo $nome . str_repeat(" ", 43 - mb_strlen($nome)) . " | ";
+            echo str_pad($row["quantidade_est"], 13, " ", STR_PAD_LEFT) . " |\n";
+            $total += $row["quantidade_est"];
         }
+        echo "+------+---------------------------------------------*---------------+\n";
         if (!$ok) {
             print("Falha ao tentar ler no banco de dados \n \n");
 
@@ -107,36 +109,25 @@ class db
     {
         $ok = pg_query($this->connection, "SELECT * FROM $table WHERE id_est = $id");
         system('clear');
-        echo "-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=\n";
-        echo "|  ID  |       Nome      | Quantidade |\n";
-        echo "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-\n\n";
-
+        echo "+------+---------------------------------------------+---------------+\n";
+        echo "|  ID  |                   Nome                      |     Estoque   |\n";
+        echo "+------+---------------------------------------------+---------------+\n";
+        $total = 0;
         while ($row = pg_fetch_assoc($ok)) {
+            $nome = mb_strcut($row["nome_est"], 0, 42);
             echo "| " . str_pad($row["id_est"], 4, " ", STR_PAD_LEFT) . " | ";
-            echo str_pad(substr($row["nome_est"], 0, 18), 15, " ", STR_PAD_RIGHT) . " | ";
-            echo str_pad($row["quantidade_est"], 10, " ", STR_PAD_RIGHT) . " |\n";
-            echo "_______________________________________\n\n";
+            echo $nome . str_repeat(" ", 43 - mb_strlen($nome)) . " | ";
+            echo str_pad($row["quantidade_est"], 13, " ", STR_PAD_LEFT) . " |\n";
+            $total += $row["quantidade_est"];
         }
+        echo "+------+---------------------------------------------*---------------+\n";
         if (!$ok) {
             print("Falha ao tentar ler no banco de dados \n \n");
 
             readline('Pressione qualquer tecla para continuar');
         }
     }
-    public function lerMovimentacaoEspecifico($id)
-    {
-        $query = pg_query($this->connection, "SELECT * FROM movimentacao WHERE fkitemestoque_est = $id");
-        $ok = pg_query($this->connection, "SELECT * FROM estoque WHERE id_est = $id");
-        $queryMovimentacao = pg_fetch_all($query);
-        $ok = pg_fetch_all($ok);
-        $this->relatorioMovimentacaoEspecifico($id);
 
-        if (!$query) {
-            echo "Falha ao tentar ler no banco de dados\n\n";
-
-            readline('Pressione qualquer tecla para continuar');
-        }
-    }
     public function relatorioTotal()
     {
         $selectAll = pg_query($this->connection, "SELECT * FROM estoque");
@@ -149,7 +140,6 @@ class db
         } else {
             readline('Pressione qualquer tecla para continuar');
         }
-
     }
     public function relatorioEspecifico($id)
     {
@@ -163,24 +153,25 @@ class db
         } else {
             readline('Pressione qualquer tecla para continuar');
         }
-
     }
-    public function relatorioMovimentacaoEspecifico($id)
+    public function relatorioMovimentacao()
     {
 
-        $selectOne = pg_query($this->connection, "SELECT * FROM movimentacao WHERE  id_mov= $id ");
-        $select = pg_fetch_all($selectOne);
+        $selectAll = pg_query($this->connection, "SELECT * FROM movimentacao ");
+        $select = pg_fetch_all($selectAll);
 
-        echo "\033c";
-        echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n";
-        echo "|  ID  |  Tipo de Transação  |  Quantidade  |  Data              |\n";
-        echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n";
-        while ($row = pg_fetch_assoc($selectOne)) {
+        echo "+------+-------------------+----------------------+------------+\n";
+        echo "|  ID  | Data de Movimento |   Tipo de Movimento  | Quantidade |\n";
+        echo "+------+-------------------+----------------------+------------+\n";
+        $total = 0;
+        while ($row = pg_fetch_assoc($selectAll)) {
             echo "| " . str_pad($row["id_mov"], 4, " ", STR_PAD_LEFT) . " | ";
-            echo str_pad(substr($row["tipotransacao_mov"], 0, 18), 10, " ", STR_PAD_RIGHT) . " | ";
-            echo str_pad($row["quantidade_mov"], 12, " ", STR_PAD_LEFT) . " | ";
-            echo str_pad($row["data_mov"], 20, " ", STR_PAD_RIGHT) . " |\n";
+            echo str_pad(date_format(date_create($row["data_mov"]), "d/m/Y"), 17, " ", STR_PAD_RIGHT) . " | ";
+            echo str_pad($row["tipotransacao_mov"], 27 - strlen($row["tipotransacao_mov"]), " ", STR_PAD_RIGHT) . " | ";
+            echo str_pad($row["quantidade_mov"], 10, " ", STR_PAD_LEFT) . " |\n";
+            $total += $row["quantidade_mov"];
         }
+        echo "+------+-------------------+----------------------+------------+\n";
         echo "\nDeseja exportar o relatório?\n[1] Não\n[2] Exportar relatório\nDigite sua opção:    ";
         $exportar = readline();
         if ($exportar == 2) {
@@ -188,41 +179,34 @@ class db
         } else {
             readline('Pressione qualquer tecla para continuar');
         }
-
     }
-    public function relatorioMovimentoData($id, $dataInicial, $dataFinal)
+    public function relatorioMovimentoData($dataInicial, $dataFinal)
     {
-        $selectOne = pg_query($this->connection, "SELECT * FROM movimentacao WHERE fkItemEstoque_est = '$id' AND data_mov BETWEEN '$dataInicial' AND '$dataFinal'");
-
-        $selectOne = pg_fetch_all($selectOne);
-        $this->relatorioMovimentacaoEspecifico($id);
-        // echo "\nDeseja exportar o relatório?\n[1] Não\n[2] Exportar relatório\nDigite sua opção:    ";
-        // $exportar = readline();
-        // if ($exportar == 2) {
-        //     $this->csv($selectOne);
-        // } else {
-        //     readline('Pressione qualquer tecla para continuar');
-        // }
-    }
-
-    public function movimentoGeral()
-    {
-        $selectOne = pg_query($this->connection, "SELECT * FROM movimentacao ");
-
-        echo "\033c";
-        echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n";
-        echo "|  ID  |  Tipo de Transação  |  Quantidade  |  Data              |\n";
-        echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n";
+        $selectOne = pg_query($this->connection, "SELECT * FROM movimentacao WHERE data_mov BETWEEN '$dataInicial' AND '$dataFinal'");
+        $select = pg_fetch_all($selectOne);
+        echo "+------+-------------------+----------------------+------------+\n";
+        echo "|  ID  | Data de Movimento |   Tipo de Movimento  | Quantidade |\n";
+        echo "+------+-------------------+----------------------+------------+\n";
+        $total = 0;
         while ($row = pg_fetch_assoc($selectOne)) {
             echo "| " . str_pad($row["id_mov"], 4, " ", STR_PAD_LEFT) . " | ";
-            echo str_pad(substr($row["tipotransacao_mov"], 0, 18), 10, " ", STR_PAD_RIGHT) . " | ";
-            echo str_pad($row["quantidade_mov"], 12, " ", STR_PAD_LEFT) . " | ";
-            echo str_pad($row["data_mov"], 20, " ", STR_PAD_RIGHT) . " |\n";
+            echo str_pad(date_format(date_create($row["data_mov"]), "d/m/Y"), 17, " ", STR_PAD_RIGHT) . " | ";
+            echo str_pad($row["tipotransacao_mov"], 27 - strlen($row["tipotransacao_mov"]), " ", STR_PAD_RIGHT) . " | ";
+            echo str_pad($row["quantidade_mov"], 10, " ", STR_PAD_LEFT) . " |\n";
+            $total += $row["quantidade_mov"];
+        }
+        echo "+------+-------------------+----------------------+------------+\n";
+        echo "\nDeseja exportar o relatório?\n[1] Não\n[2] Exportar relatório\nDigite sua opção:    ";
+        $exportar = readline();
+        if ($exportar == 2) {
+            $this->csv($select);
+        } else {
+            readline('Pressione qualquer tecla para continuar');
         }
     }
     public function csv($dados, $nomeArquivo = 'dados')
     {
-        $arquivo = fopen($nomeArquivo . "(" . random_string(5) . ').csv', 'w');
+        $arquivo = fopen($nomeArquivo . "(" . $this->random_string(5) . ').csv', 'w');
 
         if ($arquivo) {
             foreach ($dados as $linha) {
@@ -234,18 +218,14 @@ class db
             echo "Erro ao abrir o arquivo para escrita.";
         }
     }
-
-
-
-}
-function random_string($length)
-{
-    $rand_string = '';
-    for ($i = 0; $i < $length; $i++) {
-        $number = random_int(0, 36);
-        $character = base_convert($number, 10, 36);
-        $rand_string .= $character;
+    public function random_string($length)
+    {
+        $rand_string = '';
+        for ($i = 0; $i < $length; $i++) {
+            $number = mt_rand(0, 36);
+            $character = base_convert($number, 10, 36);
+            $rand_string .= $character;
+        }
+        return $rand_string;
     }
-
-    return $rand_string;
 }
--- 4 - Treinamento/index.php
+++ 4 - Treinamento/index.php
@@ -6,90 +6,147 @@ $db = new db();
 
 $db->connect();
 while (true) {
-
     echo "\n[1] Cadastro \n[2] Movimentação\n[3] Gerar relatório\nInsira: ";
     $selecionarMenu = readline();
 
     system('clear');
 
-    switch ($selecionarMenu) {
-        case 1:
-            cadastrar($db);
-            break;
-        case 2:
-            movimentacao($db);
-            break;
-        case 3:
-            gerarRelatorio($db);
-            break;
+    if ($selecionarMenu === '1' || $selecionarMenu === '2' || $selecionarMenu === '3') {
+        switch ($selecionarMenu) {
+            case '1':
+                cadastrar($db);
+                break;
+            case '2':
+                movimentacao($db);
+                break;
+            case '3':
+                gerarRelatorio($db);
+                break;
+        }
+    } else {
+        echo "Entrada inválida. Insira apenas os valores 1, 2 ou 3.\n";
     }
 }
 function cadastrar($db)
 {
     echo "-=-=-=-=-=-=-=-= Cadastrar produto -=-=-=-=-=-=-=-=\n";
-    echo "Insira o nome:   ";
-    $nome_est = readline();
+    $nome_est = "";
+    $quantidade_est = "";
 
-    echo "Insira a quantidade: ";
-    $quantidade_est = readline();
+    while (true) {
+        echo "Insira o nome:   ";
+        $nome_est = readline();
+
+        if (ctype_alpha($nome_est)) {
+            break;
+        } else {
+            echo "Erro: O nome deve conter apenas letras. Tente novamente.\n";
+        }
+    }
+    while (true) {
+        echo "Insira a quantidade: ";
+        $quantidade_est = readline();
+
+        if (ctype_digit($quantidade_est)) {
+            $quantidade_est = intval($quantidade_est);
+            break;
+        } else {
+            echo "Erro: A quantidade deve ser um número inteiro. Tente novamente.\n";
+        }
+    }
 
     $db->cadastrar('estoque', ['nome_est' => $nome_est, 'quantidade_est' => $quantidade_est]);
 }
 function movimentacao($db)
 {
-    system('clear');
-    $db->lerEstoque('estoque');
-    echo "Qual é o tipo da transação: \n[0] Entrada [1] Saída\nInsira:";
-    $tipotransacao_mov = readline();
-    if ($tipotransacao_mov == 1) {
+    while (true) {
+        system('clear');
+        $db->lerEstoque('estoque');
 
-        $tipotransacao_mov = "Saída";
-    } else {
-        $tipotransacao_mov = "Entrada";
+        $validInput = false;
+        while (!$validInput) {
+            echo "Qual é o tipo da transação: \n[0] Entrada [1] Saída [9] Voltar ao menu principal\nInsira:";
+            $tipotransacao_mov = readline();
+
+            if ($tipotransacao_mov === '0' || $tipotransacao_mov === '1' || $tipotransacao_mov === '9') {
+                $validInput = true;
+            } else {
+                system('clear');
+                $db->lerEstoque('estoque');
+                echo "Entrada inválida. Apenas os valores 0, 1 ou 9 são permitidos.\n";
+            }
+        }
+
+        if ($tipotransacao_mov === '9') {
+            return; // Volta ao menu principal
+        }
+
+        $tipotransacao_mov = $tipotransacao_mov === '1' ? "Saída" : "Entrada";
+        $today = (date("Y-m-d"));
+
+        echo "Insira qual item deseja: ";
+        $fkItemEstoque_est = readline();
+
+        echo "Insira a quantidade: ";
+        $quantidade_mov = readline();
+        $venda_mov = 0;
+
+        if (!is_numeric($fkItemEstoque_est) || !is_numeric($quantidade_mov)) {
+            echo "Entrada inválida. Apenas números são permitidos.\n";
+            return;
+        }
+
+        $db->movimentar('movimentacao', ['tipotransacao_mov' => $tipotransacao_mov, 'data_mov' => $today, 'quantidade_mov' => $quantidade_mov, 'fkItemEstoque_est' => $fkItemEstoque_est], $fkItemEstoque_est);
     }
-    $today = (date("Y-m-d"));
-    echo "Insira qual item deseja: ";
-    $fkItemEstoque_est = readline();
-
-    echo "Insira a quantidade: ";
-    $quantidade_mov = readline();
-    $venda_mov = 0;
-
-    $db->movimentar('movimentacao', ['tipotransacao_mov' => $tipotransacao_mov, 'data_mov' => $today, 'quantidade_mov' => $quantidade_mov, 'fkItemEstoque_est' => $fkItemEstoque_est], $fkItemEstoque_est);
 }
 function gerarRelatorio($db)
 {
-    echo "[1] Relatório geral do estoque \n[2] Relatório especifico do estoque\n[3] Relatório geral do movimento\n[4] Relatório movimento com data\nInsira: ";
-    $selecionarMenu = readline();
+    while (true) {
+        echo "[1] Relatório geral do estoque \n[2] Relatório especifico do estoque\n[3] Relatório geral do movimento\n[4] Relatório movimento com data\n[0] Voltar ao menu principal\nInsira: ";
+        $selecionarMenu = readline();
 
-    system('clear');
+        system('clear');
 
-    switch ($selecionarMenu) {
-        case 1:
-            $db->relatorioTotal();
-            break;
-        case 2:
-            $db->lerEstoque('estoque');
-            echo "Insira o id: ";
-            $id = readline();
-            $db->relatorioEspecifico($id);
-            break;
-        case 3:
-            $db->movimentoGeral();
-            echo "Insira o id: ";
-            $id = readline();
-            $db->lerMovimentacaoEspecifico($id);
-            break;
-        case 4:
-            $db->movimentoGeral();
-            echo "Insira o id: ";
-            $id = readline();
-            echo "Data inicial: ";
-            $dataInicial = readline();
-            echo "Data final: ";
-            $dataFinal = readline();
-
-            $db->relatorioMovimentoData($id, $dataInicial, $dataFinal);
-            break;
+        switch ($selecionarMenu) {
+            case 1:
+                $db->relatorioTotal();
+                break;
+            case 2:
+                $db->lerEstoque('estoque');
+                $validInput = false;
+                while (!$validInput) {
+                    echo "Insira o id: ";
+                    $id = readline();
+                    if (is_numeric($id)) {
+                        $validInput = true;
+                    } else {
+                        echo "Entrada inválida. Apenas números são permitidos.\n";
+                    }
+                }
+                $db->relatorioEspecifico($id);
+                break;
+            case 3:
+                $db->relatorioMovimentacao();
+                break;
+            case 4:
+                $validInput = false;
+                while (!$validInput) {
+                    echo "Data inicial: ";
+                    $dataInicial = readline();
+                    echo "Data final: ";
+                    $dataFinal = readline();
+                    if (is_numeric($dataInicial) && is_numeric($dataFinal)) {
+                        $validInput = true;
+                    } else {
+                        echo "Entrada inválida. Apenas números são permitidos.\n";
+                    }
+                }
+                $db->relatorioMovimentoData($dataInicial, $dataFinal);
+                break;
+            case 0:
+                return; // Volta ao menu principal
+            default:
+                echo "Opção inválida. Insira uma opção válida.\n";
+        }
     }
 }
